68e9c8303739f8a3a04bee1da69895a0e9cc8423
xiaomiao22 <miaogx@yeah.net>
2016-08-13 21:22:48 +0800

    second commit

---
+++ manobject1/src/sys/man_type.h
@@ -0,0 +1,9 @@
+#ifndef _MAN_TYPE_H_INCLUDE_
+#define _MAN_TYPE_H_INCLUDE_
+
+typedef intptr_t		man_int_t;
+typedef uintptr_t		man_uint_t;
+typedef intptr_t		man_flag_t;
+
+
+#endif /*_MAN_TYPE_H_INCLUDE_*/
